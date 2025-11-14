158a90474e19581caa297fa150bd564e57a4d4fe
JoanMari1C <joanmaritorrent@cev.com>
2025-11-14 17:03:02 +0100

    Prueba 3 creo

--- Assets/_AMyAssets/Scripts/Managers/PlayerRegistry.cs
+++ Assets/_AMyAssets/Scripts/Managers/PlayerRegistry.cs
@@ -2,12 +2,10 @@ using System.Collections.Generic;
 using System.Linq;
 using PurrNet;
 
-public class PlayerRegistry 
+public static class PlayerRegistry
 {
-    // Lista de todos los jugadores
     public static List<Player> AllPlayers = new();
 
-    // Devuelve el Player local correspondiente al PlayerID
     public static Player GetLocalPlayer(PlayerID id)
     {
         return AllPlayers.FirstOrDefault(p => p.owner.Value == id);
